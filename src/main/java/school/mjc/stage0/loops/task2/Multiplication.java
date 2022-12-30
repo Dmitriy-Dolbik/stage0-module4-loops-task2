64ff120fdf774e3e6f16e3c5987c4a5819146fc4
Dmitrii Dolbik <dda1508@gmail.com>
2022-12-30 17:22:22 +0300

    correct solution4

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,10 +3,13 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public static void main(String[] args) {
         Multiplication multiplication = new Multiplication();
-        multiplication.printMultiplied(-5);
+        multiplication.printMultiplied(5);
     }
 
     public void printMultiplied(int multiplyByAndToInclusive) {
+        if (multiplyByAndToInclusive == 0){
+            return;
+        }
         int sum = 0;
         int counter = 0;
         while (counter <= Math.abs(multiplyByAndToInclusive)) {
